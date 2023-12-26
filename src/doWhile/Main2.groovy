e5368c04ba561c179ecda4de404ef2bb40ebcd76
lucas mateus <lucasmss16@gmail.com>
2023-12-26 15:37:09 -0300

    Implementando DoWhile

---
+++ src/doWhile/Main2.groovy
@@ -0,0 +1,14 @@
+package doWhile
+
+ static void main(String[] args) {
+   int idade = 0;
+
+     //Diferente do while o Do Whilhe executa pirmeiro e verifica depois
+     //Executa o Do até que o whilhe seja falso
+     do {
+       println("Menor de idade " + idade)
+
+       idade++
+   } while (idade < 18)
+
+  }
